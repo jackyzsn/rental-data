f50a6c9bd85c92cb6c64cbac85916d4c9bd1322e
Jacky Zhang <Jacky.zhang>
2021-05-30 13:56:30 -0400

    change log real ip

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ try {
 
 app.use(
   logger(
-    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent"'
+    ':req[x-forwarded-for] [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms ":referrer" ":user-agent"'
   )
 );
 app.use(express.json());
